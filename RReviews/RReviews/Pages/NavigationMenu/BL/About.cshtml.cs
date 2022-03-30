0f0ada1acf8eacca3fd33a5ca604063d097aeb76
ergo lance <brainispower21@gmail.com>
2022-03-30 11:29:34 +0300

    Add About Page. Get rid of dropdown menu in navigation bar.

---
+++ RReviews/RReviews/Pages/NavigationMenu/BL/About.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RReviews.Pages.NavigationMenu.BL
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
