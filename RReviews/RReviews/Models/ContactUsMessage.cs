9238f9be9e53b4e17a35a1f6704fab0d9820d494
ergo lance <brainispower21@gmail.com>
2022-03-30 17:13:01 +0300

    Add Display attributes to modules. Add ObjectName to html forms.

--- RReviews/RReviews/Models/ContactUsMessage.cs
+++ RReviews/RReviews/Models/ContactUsMessage.cs
@@ -11,19 +11,24 @@ namespace RReviews.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "FirstName")]
         [Required]
         public string FirstName { get; set; } = string.Empty;
 
+        [Display(Name = "LastName")]
         [Required]
         public string LastName { get; set; } = string.Empty;
 
+        [Display(Name = "Email")]
         [Required]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; } = string.Empty;
 
+        [Display(Name = "PhoneNumber")]
         [DataType(DataType.PhoneNumber)]
         public string? PhoneNumber { get; set; }
 
+        [Display(Name = "Text")]
         public string? AdditionalInformation { get; set; }
     }
 }
--- RReviews/RReviews/Models/Review.cs
+++ RReviews/RReviews/Models/Review.cs
@@ -16,23 +16,29 @@ namespace RReviews.Models
         [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; } = string.Empty;
 
+        [Display(Name = "Date")]
         [DataType(DataType.Date)]
         [Column(TypeName = "date")]
         public DateTime TestingDate { get; set; }
 
+        [Display(Name = "Name")]
         public string ObjectName { get; set; } = string.Empty;
 
+        [Display(Name = "Text")]
         [DataType(DataType.MultilineText)]
         public string Content { get; set; } = string.Empty;
 
+        [Display(Name = "Rating")]
         [Range(0, 10, ErrorMessage = "Allowed rating in 0..10")]
         public int ReviewRating { get; set; }
 
+        [Display(Name = "Cost")]
         [Range(0.0, double.MaxValue, ErrorMessage = "Value should be positive.")]
         [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Cost { get; set; }
 
+        [Display(Name = "Type")]
         [EnumDataType(typeof(ReviewObject), ErrorMessage = "Review object doesn't exist within enum")]
         public ReviewObject ReviewTypeObject { get; set; }
     }
